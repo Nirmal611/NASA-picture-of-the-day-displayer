f80ca05aad06160cebc24dc3e166e9384103c9b8
gitmal <pnirmal896@gmail.com>
2024-09-13 17:01:54 +0530

    add a select date feature

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 import streamlit as st
 from nasaapi import img_of_the_day
+import datetime
 
-
-title,explanation,date =img_of_the_day()
-st.title(title)
-st.write(date)
-st.image('img.jpeg')
-st.write(explanation)
+max_date = datetime.datetime.now()
+min_date = datetime.date(2023,1,1)
+st.title('NASA astronomy image of the day '.title())
+date = st.date_input('Select a day', key='date', value=None, format='YYYY-MM-DD', max_value=max_date, min_value=min_date)
+print(date)
+if date :
+    title, explanation = img_of_the_day(date)
+    st.subheader(title)
+    st.write(date)
+    st.image('img.jpeg')
+    st.write("\t"+explanation)
--- nasaapi.py
+++ nasaapi.py
@@ -1,19 +1,17 @@
 import requests
 
 api_key = 'abcRefWSP3w1LRHjvt7jCxBBJSZibbcwrAwkfvSS'
-url = f'https://api.nasa.gov/planetary/apod?api_key={api_key}'
 
-def img_of_the_day():
+def img_of_the_day(date):
+    url = f'https://api.nasa.gov/planetary/apod?api_key={api_key}&date={date}'
     response = requests.get(url)
-    print(response.content)
-    response = response.json()
     print(response)
+    response = response.json()
     title = response['title']
-    date = response['date']
     explanation = response['explanation']
     image = requests.get(response['hdurl'])
     image = image.content
     with open('img.jpeg','wb') as img :
         img.write(image)
 
-    return [title, explanation, date]
+    return [title, explanation]
