5b67ce8326a1a3bb408ace16ef26fd169563bc84
Nirmal611 <125339075+Nirmal611@users.noreply.github.com>
2024-09-17 19:54:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,13 +2,14 @@ import streamlit as st
 from nasaapi import img_of_the_day
 import datetime
 
+api_key = st.secrets['API_KEY']
 max_date = datetime.datetime.now()
 min_date = datetime.date(2023,1,1)
 st.title('NASA astronomy image of the day '.title())
 date = st.date_input('Select a day', key='date', value=None, format='YYYY-MM-DD', max_value=max_date, min_value=min_date)
 print(date)
 if date :
-    title, explanation = img_of_the_day(date)
+    title, explanation = img_of_the_day(api_key,date)
     st.subheader(title)
     st.write(date)
     st.image('img.jpeg')
